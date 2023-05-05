b32c62d0e91fd76f0e43ba89b72baaa7ed6c79b7
Sing Wong <eiffel@Sings-MacBook-Pro.local>
2023-05-04 19:14:21 -0700

    fixed bug

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -8,7 +8,6 @@ import { RemixBrowser } from "@remix-run/react";
 import { startTransition, StrictMode } from "react";
 import { hydrateRoot } from "react-dom/client";
 
-
 startTransition(() => {
   hydrateRoot(
     document,
--- app/root.tsx
+++ app/root.tsx
@@ -19,7 +19,6 @@ export default function App() {
       <body>
         <Outlet />
         <ScrollRestoration />
-        <Scripts />
         <LiveReload />
       </body>
     </html>
