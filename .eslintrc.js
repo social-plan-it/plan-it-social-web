f6fb498a12c43cf75a262a5181d4eca1c10aff16
Andre <andre.timo.landgraf@gmail.com>
2023-05-16 14:03:36 -0700

    ESLint and Prettier setup with docs (#10)
    * eslint and prettier setup with docs
    
    * fix slow eslint command, enhance docs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,38 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const config = require('./.prettierrc');
+
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  extends: ["@remix-run/eslint-config", "@remix-run/eslint-config/node"],
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+    '@remix-run/eslint-config',
+    '@remix-run/eslint-config/node',
+  ],
+  plugins: ['@typescript-eslint', 'html', 'jsx-a11y'],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true, // Allows for the parsing of JSX
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
+    },
+  },
+  rules: {
+    'prettier/prettier': ['error', config],
+    '@typescript-eslint/consistent-type-imports': [
+      'error',
+      {
+        prefer: 'type-imports',
+        disallowTypeAnnotations: true,
+      },
+    ],
+  },
 };
