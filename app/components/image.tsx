8957dc635573ff4e58a15b34951924acd0407b3e
Andre Landgraf <andre.timo.landgraf@gmail.com>
2023-06-19 19:08:37 -0700

    fix encoding, add query params

--- app/components/image.tsx
+++ app/components/image.tsx
@@ -2,8 +2,9 @@ import type { ImgHTMLAttributes } from 'react';
 
 type ImageProps = ImgHTMLAttributes<HTMLImageElement> & {
   alt: string;
+  src: string;
 };
 
 export function Image({ alt, src, ...props }: ImageProps) {
-  return <img alt={alt} src={`/_vercel/image?url=${src}`} {...props} />;
+  return <img alt={alt} src={`/_vercel/image?url=${encodeURIComponent(src)}&w=750&q=75`} {...props} />;
 }
