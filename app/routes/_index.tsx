53eba6a4a62530b152b30ef13e8a5b643c6eade8
Shamil Khaibullov <sh.khaibullov@gmail.com>
2023-06-27 19:51:14 +0300

    delete the card component and the cards section

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,8 +1,6 @@
 import type { V2_MetaFunction } from "@remix-run/react";
 import EventsSection from "../components/events-section";
 
-import CardsSection from "~/components/cards-section";
-
 export const meta: V2_MetaFunction = () => {
   return [{ title: "Plan It Social" }];
 };
@@ -11,7 +9,6 @@ export default function Index() {
   return (
     <>
       <EventsSection />
-      <CardsSection />
     </>
   );
 }
