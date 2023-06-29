406ead5a2842df091b0a9875b9736548cf76c203
Shamil Khaibullov <sh.khaibullov@gmail.com>
2023-06-29 17:29:07 +0300

    create responsive upcoming events section

--- app/components/events-section.tsx
+++ app/components/events-section.tsx
@@ -1,104 +1,70 @@
 export default function EventsSection() {
   return (
-    <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-      <div className="grid gap-8 lg:grid-cols-3 sm:max-w-sm sm:mx-auto lg:max-w-full">
-        <div className="overflow-hidden rounded-lg shadow-sm bg-sky-950">
-          <img
-            src="https://res.cloudinary.com/djqdtvv7u/image/upload/v1684784673/web-meeting_shwwpl.jpg"
-            className="object-cover w-full h-64 pt-5 px-5"
-            alt=""
-          />
-          <div className="p-5 border-t-0 text-white">
-            <p className="mb-3 text-xs font-semibold tracking-wide uppercase">
-              <a
-                href="/"
-                className="transition-colors duration-200 text-blue-gray-900 hover:text-deep-purple-accent-700"
-                aria-label="Category"
-                title="traveling"
-              >
-                Mon, May 18 - 5:00 pm PDT
-              </a>
-            </p>
-            <a
-              href="/"
-              aria-label="Category"
-              title="Tech Talks: Exploring the Latest Web Development Trends"
-              className="inline-block mb-3 text-2xl font-bold leading-7 transition-colors duration-200 hover:text-deep-purple-accent-700"
-            >
-              Tech Talks: Exploring the Latest Web Development Trends
-            </a>
-            <p className="mb-2">
-              Join us for an evening of insightful talks on progressive web
-              apps, serverless architecture, and the future of JavaScript
-              frameworks.
-            </p>
-          </div>
-        </div>
-        <div className="overflow-hidden rounded-lg shadow-sm bg-sky-950 flex flex-col">
-          <div className="p-5 border-t-0 text-white flex-grow">
-            <p className="mb-3 text-xs font-semibold tracking-wide uppercase">
-              <a
-                href="/"
-                className="transition-colors duration-200 text-blue-gray-900 hover:text-deep-purple-accent-700"
-                aria-label="Category"
-                title="traveling"
-              >
-                Mon, May 22 - 6:00 pm PDT
-              </a>
-            </p>
-            <a
-              href="/"
-              aria-label="Category"
-              title="Fitness Bootcamp: Get Fit and Stay Active"
-              className="inline-block mb-3 text-2xl font-bold leading-7 transition-colors duration-200 hover:text-deep-purple-accent-700"
-            >
-              Fitness Bootcamp: Get Fit and Stay Active
-            </a>
-            <p className="mb-2">
-              Break a sweat and achieve your fitness goals with our
-              high-intensity workout session suitable for all fitness levels.
-            </p>
-          </div>
-          <div className="h-1/2">
+    <section className="max-w-screen-xl bg-[#E4EFF0]">
+      <div className="sm:ml-24 pt-10 pb-20 space-y-3 ">
+        <h2 className="text-center sm:text-left sm:text-5xl text-4xl font-bold italic text-[#1F2937]">
+          Upcoming Events
+        </h2>
+        <div className="flex flex-wrap gap-6 justify-center sm:justify-start">
+          <div className="flex flex-col w-[350px] mx-6 sm:mx-0 max-w-md p-6 rounded-2xl bg-[#1F2937]">
             <img
-              src="https://res.cloudinary.com/djqdtvv7u/image/upload/v1684784670/fitness-meeting_b6vtmv.jpg"
-              className="object-cover w-full h-full pb-5 px-5"
-              alt=""
+              src="upc-events1.png"
+              className="w-full object-contain box-border"
+              alt="group of people in a meeting"
+            />
+            <div className="pt-6 text-lg sm:text-2xl font-bold text-white">
+              Group Name
+            </div>
+            <h3 className="text-3xl sm:text-4xl font-bold text-white">
+              Event Title
+            </h3>
+            <p className="w-3/5 text-lg sm:text-2xl text-white">
+              Event description blah blah blah blah blah blah blah blah blah
+            </p>
+            <div className="text-lg sm:text-2xl font-bold text-white">
+              Mon, May 18 - 5:00 pm PDT
+            </div>
+          </div>
+          <div className="flex flex-col w-[350px] mx-6 sm:mx-0 max-w-md p-6 rounded-2xl bg-[#1F2937]">
+            <div className="text-lg sm:text-2xl font-bold text-white">
+              Group Name
+            </div>
+            <h3 className="text-3xl sm:text-4xl font-bold text-white">
+              Event Title
+            </h3>
+            <p className="w-3/5 text-lg sm:text-2xl text-white">
+              Event description blah blah blah blah blah blah blah blah blah
+            </p>
+            <div className="text-lg sm:text-2xl font-bold text-white">
+              Mon, May 18 - 5:00 pm PDT
+            </div>
+            <img
+              src="upc-events2.png"
+              className="pt-8 w-full object-contain box-border"
+              alt="group of people in a meeting"
             />
           </div>
-        </div>
-        <div className="overflow-hidden rounded-lg shadow-sm bg-sky-950">
-          <img
-            src="https://res.cloudinary.com/djqdtvv7u/image/upload/v1684784671/art-meeting_uo67yc.jpg"
-            className="object-cover w-full h-64 pt-5 px-5"
-            alt=""
-          />
-          <div className="p-5 border-t-0 text-white">
-            <p className="mb-3 text-xs font-semibold tracking-wide uppercase">
-              <a
-                href="/"
-                className="transition-colors duration-200 text-blue-gray-900 hover:text-deep-purple-accent-700"
-                aria-label="Category"
-                title="traveling"
-              >
-                Thu, May 26 - 8:00 am PDT
-              </a>
+          <div className="flex flex-col w-[350px] mx-6 sm:mx-0 max-w-md p-6 rounded-2xl bg-[#1F2937]">
+            <img
+              src="upc-events3.png"
+              className="w-full object-contain box-border"
+              alt="group of people in a meeting"
+            />
+            <div className="pt-6 text-lg sm:text-2xl font-bold text-white">
+              Group Name
+            </div>
+            <h3 className="text-3xl sm:text-4xl font-bold text-white">
+              Event Title
+            </h3>
+            <p className="w-3/5 text-lg sm:text-2xl text-white">
+              Event description blah blah blah blah blah blah blah blah blah
             </p>
-            <a
-              href="/"
-              aria-label="Category"
-              title="Art Workshop: Discover Your Creative Side"
-              className="inline-block mb-3 text-2xl font-bold leading-7 transition-colors duration-200 hover:text-deep-purple-accent-700"
-            >
-              Art Workshop: Discover Your Creative Side
-            </a>
-            <p className="mb-2">
-              Unleash your creativity and create your own masterpiece in a
-              supportive and inspiring art workshop.
-            </p>
+            <div className="text-lg sm:text-2xl font-bold text-white">
+              Mon, May 18 - 5:00 pm PDT
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,11 @@ import type { Config } from "tailwindcss";
 export default {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
