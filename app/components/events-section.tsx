f8c4439fb636f1d489613ed5d1a561b68c5e4053
Shamil Khaibullov <sh.khaibullov@gmail.com>
2023-08-09 18:21:50 +0300

    change prisma model, mock data, and improve EventCard component

--- app/components/events-section.tsx
+++ app/components/events-section.tsx
@@ -1,66 +1,48 @@
-type Event = {
-  id: number;
-  imgUrl: string;
-  imgAlt: string;
-  groupName: string;
-  eventTitle: string;
-  eventDescription: string;
-  date: string;
-  time: string;
-  location: string;
-  category: string;
-};
+import type { Event, Group } from '@prisma/client';
 
-export type EventsSectionProps = {
+export type EventsListProps = {
   events: Event[];
+  groups: Group[];
 };
 
-export type EventPreviewProps = {
-  event: Event;
-};
-
-export function EventsSection({ events }: EventsSectionProps) {
+export function EventsSection({ events, groups }: EventsListProps) {
   return (
     <section className="bg-secondary">
       <div className="sm:ml-24 pt-10 pb-20 space-y-3">
         <h2 className="text-center sm:text-left sm:text-5xl text-4xl font-bold italic text-primary">Upcoming Events</h2>
         <div className="flex flex-wrap gap-6 justify-center sm:justify-start">
-          {events.map((event, index) => (
-            <div
-              key={event.id}
-              className="flex flex-col space-y-3 w-[350px] mx-6 sm:mx-0 max-w-md p-6 rounded-2xl bg-primary"
-            >
-              {index === 1 ? (
-                <>
-                  <div className="text-lg sm:text-2xl font-bold text-white">{event.groupName}</div>
-                  <h3 className="text-3xl sm:text-4xl font-bold text-white">{event.eventTitle}</h3>
-                  <p className="text-lg sm:text-2xl text-white">{event.eventDescription}</p>
-                  <div className="text-lg sm:text-2xl font-bold text-white">
-                    {event.date} - {event.time}
-                  </div>
-                  <img src={event.imgUrl} className="pt-8 w-full object-contain box-border" alt={event.eventTitle} />
-                </>
-              ) : (
-                <EventPreview event={event} />
-              )}
-            </div>
-          ))}
+          {events.map((event, index) => {
+            const group = groups.find((group) => group.id === event.groupId);
+            if (!group) return null;
+            return <EventCard key={event.id} event={event} group={group} index={index} />;
+          })}
         </div>
       </div>
     </section>
   );
 }
 
-export function EventPreview({ event }: EventPreviewProps) {
+export function EventCard({ event, group, index }: { event: Event; group: Group; index: number }) {
   return (
-    <>
-      <img src={event.imgUrl} className="w-full object-contain box-border" alt={event.eventTitle} />
-      <div className="text-lg sm:text-2xl font-bold text-white">{event.groupName}</div>
-      <h3 className="text-3xl sm:text-4xl font-bold text-white">{event.eventTitle}</h3>
-      <p className="text-lg sm:text-2xl text-white">{event.eventDescription}</p>
-      <div className="text-lg sm:text-2xl font-bold text-white">
-        {event.date} - {event.time}
-      </div>
-    </>
+    <div className="flex flex-col space-y-3 w-[350px] mx-6 sm:mx-0 max-w-md p-6 rounded-2xl bg-primary">
+      {index === 1 ? (
+        <>
+          <div className="text-lg sm:text-2xl font-bold text-white">{group.name}</div>
+          <h3 className="text-3xl sm:text-4xl font-bold text-white">{event.name}</h3>
+          <p className="text-lg sm:text-2xl text-white">{event.description}</p>
+          <div className="text-lg sm:text-2xl font-bold text-white">{event.date.toLocaleString()}</div>
+          <img src={event.imgUrl} className="pt-8 w-full object-contain box-border" alt={event.imgAlt} />
+        </>
+      ) : (
+        <>
+          <img src={event.imgUrl} className="w-full object-contain box-border" alt={event.imgAlt} />
+
+          <div className="text-lg sm:text-2xl font-bold text-white">{group.name}</div>
+          <h3 className="text-3xl sm:text-4xl font-bold text-white">{event.name}</h3>
+          <p className="text-lg sm:text-2xl text-white">{event.description}</p>
+          <div className="text-lg sm:text-2xl font-bold text-white">{event.date.toLocaleString()}</div>
+        </>
+      )}
+    </div>
   );
 }
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,50 +1,66 @@
 import type { V2_MetaFunction } from '@remix-run/react';
 import HeroSection from '~/components/hero-section';
 import { EventsSection } from '../components/events-section';
+import type { Event, Group } from '@prisma/client';
 
 export const meta: V2_MetaFunction = () => {
   return [{ title: 'Plan It Social' }];
 };
 
-export const events = [
+export const events: Event[] = [
   {
-    id: 0,
-    imgUrl: 'imgs/upc-events1.png',
-    imgAlt: 'group of people in a meeting',
-    groupName: 'Group Name 1',
-    eventTitle: 'Tech Talks: Exploring the Latest Web Development Trends',
-    eventDescription:
+    id: '0',
+    name: 'Tech Talks: Exploring the Latest Web Development Trends',
+    date: new Date('2023-07-19T08:00:00'),
+    description:
       'Join us for an evening of insightful talks on progressive web apps, serverless architecture, and the future of JavaScript frameworks.',
-    date: '2023-05-18',
-    time: '5:00 PM',
-    location: '123 Main St, City',
-    category: 'Technology',
+    imgUrl: 'imgs/upc-events1.png',
+    imgAlt: 'group of people in a meeting-1',
+    location: '123 Main St, San Francisco',
+    groupId: '345',
   },
   {
-    id: 1,
-    imgUrl: 'imgs/upc-events2.png',
-    imgAlt: 'group of people in a meeting',
-    groupName: 'Group Name 2',
-    eventTitle: 'Fitness Bootcamp: Get Fit and Stay Active',
-    eventDescription:
+    id: '1',
+    name: 'Fitness Bootcamp: Get Fit and Stay Active',
+    date: new Date('2023-08-15T18:30:00'),
+    description:
       'Break a sweat and achieve your fitness goals with our high-intensity workout session suitable for all fitness levels.',
-    date: '2023-05-22',
-    time: '6:00 PM',
-    location: '456 Elm St, City',
-    category: 'Fitness',
+    imgUrl: 'imgs/upc-events2.png',
+    imgAlt: 'group of people in a meeting-2',
+    location: '456 Elm St, Munich',
+    groupId: '346',
   },
   {
-    id: 2,
-    imgUrl: 'imgs/upc-events3.png',
-    imgAlt: 'group of people in a meeting',
-    groupName: 'Group Name 3',
-    eventTitle: 'Tennis Tournament: Fun and Competitive Matches',
-    eventDescription:
+    id: '2',
+    name: 'Tennis Tournament: Fun and Competitive Matches',
+    date: new Date('2023-09-22T12:15:00'),
+    description:
       'Participate in our annual tennis tournament and showcase your skills in exciting matches with players from the community.',
-    date: '2023-05-25',
-    time: '9:00 AM',
-    location: '789 Oak St, City',
-    category: 'Sports',
+    imgUrl: 'imgs/upc-events3.png',
+    imgAlt: 'group of people in a meeting-3',
+    location: '789 Oak St, Istanbul',
+    groupId: '347',
+  },
+];
+
+export const groups: Group[] = [
+  {
+    id: '345',
+    name: 'WebDev Enthusiasts',
+    description:
+      'Join our community of web development enthusiasts and stay up-to-date with the latest trends, technologies, and innovations in the ever-evolving world of web development.',
+  },
+  {
+    id: '346',
+    name: 'FitFam Community',
+    description:
+      "Welcome to the FitFam Community! We're dedicated to helping you achieve your fitness goals and live a healthier, more active lifestyle. Join us for high-energy bootcamp sessions that are suitable for all fitness levels.",
+  },
+  {
+    id: '347',
+    name: 'Competitive Tennis Club',
+    description:
+      'Calling all tennis enthusiasts! Our Competitive Tennis Club is your gateway to exciting and friendly matches with players who share your passion for the game. Join us for fun and challenging competitive tennis tournaments.',
   },
 ];
 
@@ -52,7 +68,7 @@ export default function Index() {
   return (
     <>
       <HeroSection />
-      <EventsSection events={events} />
+      <EventsSection events={events} groups={groups} />
     </>
   );
 }
