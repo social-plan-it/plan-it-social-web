d0ff8b3cfe284268309fd8bf9bb0c81d33ecd968
CamilingJS <jonathancamiling@gmail.com>
2023-08-10 16:51:05 -0700

    catching up repo as it well behind main, also refactoring to be less static

---
+++ app/components/marketing/benefits-section.tsx
@@ -0,0 +1,44 @@
+const benefits = [
+    {
+        title: "Join a group",
+        message: "Do what you love, meet others who love it, find your community. The rest is history!",
+        imgUrl: "https://res.cloudinary.com/dxctpvd8v/image/upload/v1687225039/SocialPlan-it/Feature-Image-1_clboti.png", 
+        imgAlt: "image description",
+        link: "/groups",
+    },
+    {
+        title: "Find an event",
+        message: "Events are happening on just about any topic you can think of, from online gaming and photography to yoga and hiking.",
+        imgUrl: "https://res.cloudinary.com/dxctpvd8v/image/upload/v1687225039/SocialPlan-it/Feature-Image-2_c5mgcb.png", 
+        imgAlt: "image description",
+        link: "/events", 
+
+    },
+    {
+        title: "Start a group",
+        message: "You don’t have to be an expert to gather people together and explore shared interests.",
+        imgUrl: "https://res.cloudinary.com/dxctpvd8v/image/upload/v1687225039/SocialPlan-it/Feature-Image-3_vpibsv.png", 
+        imgAlt: "image description",
+        link: "/groups",
+    },
+];
+
+export default function BenefitsSection() {
+    return (
+        <section className="bg-gray-800 pt-5 pb-5 text-white font-sans flex justify-center content-center">
+            <div className="max-w-screen-xl flex flex-wrap content-between justify-center gap-y-8 gap-x-16 grid-cols-3">
+                {benefits.map((benefit, index) => (
+                    <a key={index} href={benefit.link}>
+                        <div className="flex flex-col p-5 text-start max-w-xs">
+                            <div className="w-full pb-5">
+                                <img className="w-full" src={benefit.imgUrl} alt={benefit.imgAlt} />
+                            </div>
+                            <h1 className="text-4xl font-bold mb-2">{benefit.title}</h1>
+                            <p className="text-lg">{benefit.message}</p>
+                        </div>
+                    </a>
+                ))}
+            </div>
+        </section>
+    );
+}
