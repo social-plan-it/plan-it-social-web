086dc5d7bc74c7af5cd0e86042ebce8c521c52e1
Shamil Khaibullov <sh.khaibullov@gmail.com>
2023-08-16 13:41:43 +0300

    rename a var in loader and divide imported type and hook

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,7 +1,8 @@
-import { useLoaderData, type V2_MetaFunction } from '@remix-run/react';
+import { useLoaderData } from '@remix-run/react';
+import { type V2_MetaFunction } from '@remix-run/react';
+import { db } from '~/modules/database/db.server';
 import HeroSection from '~/components/marketing/hero-section';
 import { EventsSection } from '../components/marketing/events-section';
-import { db } from '~/modules/database/db.server';
 
 export const meta: V2_MetaFunction = () => {
   return [{ title: 'Plan It Social' }];
@@ -14,7 +15,7 @@ export async function loader() {
 
 export default function Index() {
   const { events } = useLoaderData<typeof loader>();
-  const serializedEvents = events.map((event) => ({
+  const deserializedEvents = events.map((event) => ({
     ...event,
     date: new Date(event.date),
   }));
@@ -22,7 +23,7 @@ export default function Index() {
   return (
     <>
       <HeroSection />
-      <EventsSection events={serializedEvents} />
+      <EventsSection events={deserializedEvents} />
     </>
   );
 }
