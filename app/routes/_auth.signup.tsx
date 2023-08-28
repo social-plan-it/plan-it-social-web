49f8a331e4c5874eda02a10d536689f53a09f3ba
Tony Dang <tony@tonydang.blog>
2023-08-28 10:19:30 -0700

    use zod to validate sign up form inputs

--- app/routes/_auth.signup.tsx
+++ app/routes/_auth.signup.tsx
@@ -1,10 +1,13 @@
 import type { LoaderArgs, ActionArgs } from '@remix-run/node';
 import { redirect } from '@remix-run/node';
 import { Form, Link, useActionData, useNavigation } from '@remix-run/react';
+import { z } from 'zod';
+
 import { Card } from '~/components/ui/containers';
 import { Input } from '~/components/ui/forms';
 import { getHash } from '~/modules/database/crypto.server';
 import { db } from '~/modules/database/db.server';
+import { badRequest } from '~/modules/response/response.server';
 import { createUserSession, getUserSession } from '~/modules/session/session.server';
 
 export async function action({ request }: ActionArgs) {
@@ -14,32 +17,56 @@ export async function action({ request }: ActionArgs) {
   const password = form.get('password');
   const confirmPassword = form.get('confirmPassword');
 
-  if (!name || !email || !password || !confirmPassword) {
-    return { status: 400, message: 'Missing required fields' };
-  }
+  const SignUpForm = z.object({
+    name: z
+      .string()
+      .trim()
+      .min(1, 'Name is required.')
+      .max(191, 'Name cannot exceed 191 max length.')
+      .transform((name) => name.replace(/\s+/g, ' ')),
+    email: z
+      .string()
+      .toLowerCase()
+      .trim()
+      .email('Invalid email.')
+      .refine(
+        async (email) => !(await db.user.findUnique({ where: { email } })),
+        'Email already in use. Is this you? Please log in instead.',
+      ),
+    password: z
+      .string()
+      .min(8, 'Password must be 8 or more characters.')
+      .max(191, 'Password cannot exceed 191 max length.'),
+    confirmPassword: z.string().refine((confirmPassword) => confirmPassword === password, 'Password must match.'),
+  });
 
-  if (password !== confirmPassword) {
-    return { status: 400, message: 'Passwords do not match' };
-  }
+  const signUpForm = await SignUpForm.safeParseAsync({ name, email, password, confirmPassword });
 
-  const cleanEmail: string = email.toString().toLowerCase().trim();
-  const user = await db.user.findUnique({ where: { email: cleanEmail } });
+  const actionData = {
+    success: false,
+    fields: { email, name },
+    fieldErrors: null,
+    formError: null,
+  };
 
-  if (user) {
-    return { status: 400, message: 'Email already in use. Is this you? Please log in instead.' };
+  if (!signUpForm.success) {
+    return badRequest({
+      ...actionData,
+      fieldErrors: signUpForm.error.format(),
+    });
   }
 
   const newUser = await db.user.create({
     data: {
-      name: name.toString(),
-      email: cleanEmail,
+      name: signUpForm.data.name,
+      email: signUpForm.data.email,
     },
   });
 
   await db.password.create({
     data: {
       userId: newUser.id,
-      hash: await getHash(password.toString()),
+      hash: await getHash(signUpForm.data.password),
     },
   });
 
@@ -66,20 +93,75 @@ export default function Component() {
         <Card>
           <Form method="post" className="w-full flex flex-col items-center justify-center gap-5">
             <h1 className="text-4xl">Sign Up</h1>
-            <Input name="email" type="email" autoComplete="email" placeholder="Email" label="Email" required />
-            <Input name="name" autoComplete="username" type="text" placeholder="Name" label="Name" required />
-            <Input name="password" type="password" placeholder="Password" label="Password" required />
+
+            <Input
+              defaultValue={actionData?.fields?.name}
+              name="name"
+              autoComplete="name"
+              type="text"
+              placeholder="Name"
+              label="Name"
+              required
+              aria-invalid={Boolean(actionData?.fieldErrors?.name)}
+              aria-errormessage={actionData?.fieldErrors?.name ? 'name-error' : undefined}
+            />
+            {actionData?.fieldErrors?.name && (
+              <p className="text-sm text-red-600" id="name-error" role="alert">
+                {actionData.fieldErrors.name._errors[0]}
+              </p>
+            )}
+
+            <Input
+              defaultValue={actionData?.fields?.email}
+              name="email"
+              type="email"
+              autoComplete="email"
+              placeholder="Email"
+              label="Email"
+              required
+              aria-invalid={Boolean(actionData?.fieldErrors?.email)}
+              aria-errormessage={actionData?.fieldErrors?.email ? 'email-error' : undefined}
+            />
+            {actionData?.fieldErrors?.email && (
+              <p className="text-sm text-red-600" id="email-error" role="alert">
+                {actionData.fieldErrors.email._errors[0]}
+              </p>
+            )}
+
+            <Input
+              name="password"
+              type="password"
+              placeholder="Password"
+              label="Password"
+              required
+              aria-invalid={Boolean(actionData?.fieldErrors?.password)}
+              aria-errormessage={actionData?.fieldErrors?.password ? 'password-error' : undefined}
+            />
+            {actionData?.fieldErrors?.password && (
+              <p className="text-sm text-red-600" id="password-error" role="alert">
+                {actionData.fieldErrors.password._errors[0]}
+              </p>
+            )}
+
             <Input
               name="confirmPassword"
               type="password"
               placeholder="Confirm Password"
               label="Confirm Password"
               required
+              aria-invalid={Boolean(actionData?.fieldErrors?.confirmPassword)}
+              aria-errormessage={actionData?.fieldErrors?.confirmPassword ? 'content-error' : undefined}
             />
+            {actionData?.fieldErrors?.confirmPassword && (
+              <p className="text-sm text-red-600" id="confirmPassword-error" role="alert">
+                {actionData.fieldErrors.confirmPassword._errors[0]}
+              </p>
+            )}
+
             <button type="submit" disabled={isPending}>
               {isPending ? 'Signing up...' : 'Sign Up'}
             </button>
-            {actionData && actionData.message && <p className="text-red-500">{actionData.message}</p>}
+
             <p>
               Have an account already? <Link to="/login">Login</Link>
             </p>
