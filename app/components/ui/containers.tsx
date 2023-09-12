eb3e614963fe733b850af5b59461de73701828fe
Tony Dang <tony@tonydang.blog>
2023-09-12 09:35:21 -0700

    Add reusable components (#66)

--- app/components/ui/containers.tsx
+++ app/components/ui/containers.tsx
@@ -1,3 +1,3 @@
 export function Card({ children }: { children: React.ReactNode }) {
-  return <section className="w-full bg-white shadow-lg rounded-lg px-2 py-6 lg:p-16">{children}</section>;
+  return <section className="w-full bg-white shadow-lg rounded-lg px-6 py-6 sm:p-16">{children}</section>;
 }
