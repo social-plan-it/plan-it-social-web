fa50e275e876c8eada98c99e545b9d86919d0f1b
Will Beaumont <39424606+nmbeau@users.noreply.github.com>
2023-10-01 11:13:22 -0700

    add error component, error boundary to root, update v2 types

--- app/root.tsx
+++ app/root.tsx
@@ -1,15 +1,25 @@
-import type { LinksFunction, LoaderArgs } from '@remix-run/node';
+import type { LinksFunction, LoaderFunctionArgs } from '@remix-run/node';
 import { json } from '@remix-run/node';
 import stylesheet from './styles/tailwind.css';
 import { TopNav } from '~/components/layout/top-nav';
-import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react';
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+  isRouteErrorResponse,
+  useRouteError,
+} from '@remix-run/react';
 import Footer from './components/layout/footer';
+import { Error } from '~/components/ui/error';
 import { getCurrentUser } from '~/modules/session/session.server';
 import { db } from './modules/database/db.server';
 
 export const links: LinksFunction = () => [{ rel: 'stylesheet', href: stylesheet }];
 
-export async function loader({ request }: LoaderArgs) {
+export async function loader({ request }: LoaderFunctionArgs) {
   const eventsPromise = db.event.findMany({ include: { group: true }, take: 24 });
   const groupsPromise = db.group.findMany({ take: 24 });
   const [events, groups] = await Promise.all([eventsPromise, groupsPromise]);
@@ -26,16 +36,68 @@ export async function loader({ request }: LoaderArgs) {
 
 export default function App() {
   return (
+    <Shell>
+      <Outlet />
+    </Shell>
+  );
+}
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+
+  if (isRouteErrorResponse(error)) {
+    if (error.status == 404) {
+      return (
+        <Shell title="An Error Occurred">
+          <Error>
+            <h1>the page you are looking for does not exist</h1>
+          </Error>
+        </Shell>
+      );
+    }
+    return (
+      <Shell title="An Error Occurred">
+        <Error>
+          <h1>
+            {error.status} {error.statusText}
+          </h1>
+          <p>{error.data}</p>
+        </Error>
+      </Shell>
+    );
+  } else if (error instanceof Error) {
+    return (
+      <Shell title="Something went wrong">
+        <Error>
+          <h1>Error</h1>
+          <p>{error?.message}</p>
+        </Error>
+      </Shell>
+    );
+  } else {
+    return (
+      <Shell title="An unknown error occurred">
+        <Error>
+          <h1>Unknown Error</h1>
+        </Error>
+      </Shell>
+    );
+  }
+}
+
+function Shell({ title, children }: { title?: string; children: React.ReactNode }) {
+  return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
+        {title && <title>{title}</title>}
         <Meta />
         <Links />
       </head>
       <body className="min-h-screen w-full flex flex-col">
         <TopNav />
-        <Outlet />
+        {children}
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,11 +1,11 @@
-import { useLoaderData } from '@remix-run/react';
-import { type V2_MetaFunction } from '@remix-run/react';
+import { isRouteErrorResponse, useLoaderData, useRouteError } from '@remix-run/react';
+import { type MetaFunction } from '@remix-run/react';
 import { db } from '~/modules/database/db.server';
 import HeroSection from '~/components/marketing/hero-section';
 import { EventsSection } from '../components/marketing/events-section';
 import { BenefitsSection } from '~/components/marketing/benefits-section';
 
-export const meta: V2_MetaFunction = () => {
+export const meta: MetaFunction = () => {
   return [{ title: 'Plan It Social' }];
 };
 
@@ -28,4 +28,30 @@ export default function Index() {
       <EventsSection events={deserializedEvents} />
     </>
   );
+}
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+
+  if (isRouteErrorResponse(error)) {
+    return (
+      <div>
+        <h1>
+          {error.status} {error.statusText}
+        </h1>
+        <p>{error.data}</p>
+      </div>
+    );
+  } else if (error instanceof Error) {
+    return (
+      <div>
+        <h1>Error</h1>
+        <p>{error.message}</p>
+        <p>The stack trace is:</p>
+        <pre>{error.stack}</pre>
+      </div>
+    );
+  } else {
+    return <h1>Unknown Error</h1>;
+  }
 }
