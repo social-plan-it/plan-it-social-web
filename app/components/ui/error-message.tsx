a12919a0fa9fb0ff8cf26b4a7f1edb5cdda1d9ee
Shamil Khaibullov <sh.khaibullov@gmail.com>
2023-11-12 15:40:03 +0300

    make font colof a bit readable

--- app/components/ui/error-message.tsx
+++ app/components/ui/error-message.tsx
@@ -6,10 +6,10 @@ export function ErrorMessage({ children }: ErrorProps) {
   return (
     <div className="m-6 flex items-center justify-center">
       <div
-        className="relative w-128 justify-center self-center rounded border border-red-400 bg-red-100 px-4 py-3 text-center text-red-700"
+        className="relative w-auto justify-center self-center rounded border border-red-600 bg-red-100 px-4 py-3 text-center"
         role="alert"
       >
-        <div className="block md:inline">{children}</div>
+        <div className="block md:inline font-medium">{children}</div>
       </div>
     </div>
   );
