eec47d16fb2bf687dc365ff1f5ad12b373dfd041
eiffelwong1 <eiffelynx@gmail.com>
2023-11-13 17:29:52 -0800

    rm div according to comments from andre

--- app/modules/events/card.tsx
+++ app/modules/events/card.tsx
@@ -3,9 +3,7 @@ import type { Event } from '@prisma/client';
 export function EventCard({ name, date, description, imgUrl, imgAlt, location }: Event) {
   return (
     <div className="grid grid-cols-3 gap-8 p-4 bg-primary text-white rounded-xl">
-      <div className="">
-        <img src={imgUrl ?? ''} alt={imgAlt ?? 'group image'}></img>
-      </div>
+      <img src={imgUrl ?? ''} alt={imgAlt ?? 'group image'}></img>
       <div className="col-span-2">
         <h1 className="font-bold text-3xl">{name}</h1>
         <h3 className="text-lg">{date.toDateString()}</h3>
