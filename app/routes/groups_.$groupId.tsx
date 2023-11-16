b7717ab5f7c9b94369ba7019bc48ccf221feec81
eiffelwong1 <eiffelynx@gmail.com>
2023-11-15 23:44:15 -0800

    rm unrelated change

--- app/routes/groups_.$groupId.tsx
+++ app/routes/groups_.$groupId.tsx
@@ -1,69 +1,21 @@
-import type { Event } from '@prisma/client';
-import type { LoaderFunction, LoaderFunctionArgs, SerializeFrom } from '@remix-run/node';
+import type { LoaderFunction } from '@remix-run/node';
 import { useParams, useLoaderData, Link } from '@remix-run/react';
 import { db } from '~/modules/database/db.server';
-import { EventCard } from '~/modules/events/card';
 import { json } from '@remix-run/node';
 
-export const loader: LoaderFunction = async ({ params }: LoaderFunctionArgs) => {
-  const group = await db.group.findFirstOrThrow({ where: { id: params.groupId }, include: { events: true } });
+export const loader: LoaderFunction = async ({ params }) => {
+  const group = await db.group.findFirstOrThrow({ where: { id: params.groupId } });
   return json({ group });
 };
 
-export function useDeserializeGroup() {
-  const data = useLoaderData() as SerializeFrom<typeof loader> | undefined;
-  const events = data?.group?.events;
-  if (!events) {
-    return data;
-  }
-  const deserializedEvents = events?.map((event: { date: string }) => ({
-    ...event,
-    date: new Date(event.date),
-  }));
-  data.group.events = deserializedEvents;
-
-  console.log(events);
-  return data;
-}
-
 export default function GroupRoute() {
-  const data = useDeserializeGroup();
+  const data = useLoaderData<typeof loader>();
 
   return (
     <>
-      <div className="bg-secondary">
-        <Link to="/groups">back to groups</Link>
-
-        <div className="flex justify-left items-center sm:justify-center">
-          <div className="p-10">
-            <img
-              className="rounded-full sm:h-32 sm:w-32"
-              src={`https://ui-avatars.com/api/?name=${data?.group?.name}`}
-              alt="group icon"
-            ></img>
-          </div>
-          <div className="p-10">
-            <h1 className="md:text-3xl md:font-extrabold">{data.group.name}</h1>
-          </div>
-        </div>
-      </div>
-
-      <div className="grid grid-cols-1 gap-4 bg-grayBackground sm:grid-cols-2">
-        <div className="rounded-xl m-10 p-10 bg-white">
-          <p>{data.group.description}</p>
-        </div>
-        <div className="m-10">
-          <h1 className="text-xl font-bold">Upcoming Events</h1>
-          {data?.group?.events &&
-            data?.group?.events.map((event: Event) => {
-              return (
-                <div key={event.id}>
-                  <EventCard {...event}></EventCard>
-                </div>
-              );
-            })}
-        </div>
-      </div>
+      <Link to="/groups">back to groups</Link>
+      <h1>{data.group.name}</h1>
+      <p>{data.group.description}</p>
     </>
   );
 }
