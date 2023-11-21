d75253ffacb1c7668b183af00028ab267615b95e
CamilingJS <jonathancamiling@gmail.com>
2023-11-21 13:51:28 -0800

    added google font to links

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,5 @@
 import type { Config } from 'tailwindcss';
-import defaultTheme from 'tailwindcss/defaultTheme.js';
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 export default {
   content: ['./app/**/*.{js,jsx,ts,tsx}'],
@@ -9,10 +9,10 @@ export default {
         primary: '#1F2937',
         secondary: '#E4EFF0',
         warm: '#E11D48',
-        grayBackground: 'rgba(217, 217, 217, 0.25);',
+        grayBackground: 'rgba(217, 217, 217, 0.25)',
       },
       fontFamily: {
-        'sans': ['"Montserrat"', ...defaultTheme.fontFamily.sans],
+        sans: ['Montserrat', ...defaultTheme.fontFamily.sans],
       }
     },
   },
