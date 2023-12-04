6de9379147ecb1d5b3a2f71fc183a91174a70e65
Shamil Khaibullov <sh.khaibullov@gmail.com>
2023-12-04 14:17:17 +0300

    a bit more design

--- app/routes/$.tsx
+++ app/routes/$.tsx
@@ -10,16 +10,19 @@ export function ErrorBoundary() {
 
   return (
     <ErrorMessage>
-      <div className="flex flex-col gap-4">
+      <div className="flex flex-col items-center py-4">
         <img
           className="h-0 w-0 collapse sm:visible sm:h-60 sm:w-60"
           src="/imgs/404-not-found.png"
           alt="Page not found"
         />
-        <h1>
-          The page <span className="text-xl">{location.pathname}</span> you are looking for does not exist.
-        </h1>
-        <Link to="/">Visit our Home page here </Link>
+        <h1 className="mt-4 text-2xl sm:text-6xl tracking-tight">Page not found</h1>
+        <p className="mt-6 text-base text-gray-600">
+          The page <span className="font-bold">{location.pathname}</span> you are looking for does not exist.
+        </p>
+        <Link to="/" className="mt-12 rounded-md bg-[#028090] hover:bg-[#018C9E] text-white px-3.5 py-2.5 ">
+          Go back home
+        </Link>
       </div>
     </ErrorMessage>
   );
