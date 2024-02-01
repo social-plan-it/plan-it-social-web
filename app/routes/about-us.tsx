49737e3ce6e870293f9e75d8b8e13eca590da932
CamilingJS <jonathancamiling@gmail.com>
2024-02-01 09:16:59 -0800

    designed and completed about us page with images and lorem ipsum copy

--- app/routes/about-us.tsx
+++ app/routes/about-us.tsx
@@ -1,3 +1,77 @@
 export default function AboutUs() {
-  return <h1 className="text-2xl my-5">The About Page For Social Plan-it</h1>;
+  return (
+    <>
+      <div className="bg-primary text-white flex flex-col justify-center content-center">
+        <div className="max-w-screen-md mx-auto sm:w-4/5 flex flex-col px-8 py-8 items-center justify-center">
+          <div className="w-[320px] flex self-start mx-auto pb-3">
+            <img
+              className="w-full"
+              src="https://res.cloudinary.com/dxctpvd8v/image/upload/v1706807323/solar-system_pzkazh.png"
+              alt="Solar system"
+            />
+          </div>
+          <h2 className="w-50 pb-3 sm:w-100 font-bold sm:font-extrabold sm:text-4xl">
+            Ham hock pork shank, chislic chicken pork chop picanha. Alcatra turkey ribeye kevin fatback leberkas.
+          </h2>
+          <h3 className="w-50 sm:text-lg sm:w-90 sm:font-semibold pb-6">
+            Shank jerky shoulder short loin corned beef chislic ham cupim. Ribeye andouille jowl, short ribs chislic
+            leberkas ham rump sirloin jerky chicken turkey ground round.
+          </h3>
+          <div className="w-full sm:w-90 sm:items-end">
+            <div className="w-full sm:w-48 text-white right-2.5 bottom-2.5 bg-warm hover:bg-gray-500 focus:ring-2 focus:outline-none focus:ring-blue-500 font-semibold rounded-lg italic text-md px-2 md:px-4 py-4 lg:text-2xl">
+              Join Group
+            </div>
+          </div>
+        </div>
+        {/* <div className="flex justify-center content-center bg-secondary">
+          <h1>TODO: Place holder for event images marquee</h1>
+        </div> */}
+        <div className="max-w-screen-md mx-auto sm:w-4/5 flex flex-col px-8 py-8 items-center justify-center">
+          <div className="w-[320px] flex self-start mx-auto pb-3">
+            <img
+              className="w-full"
+              src="https://res.cloudinary.com/dxctpvd8v/image/upload/v1706805456/target_g3b96r.png"
+              alt="Arrow in targeet"
+            />
+          </div>
+          <div className="pb-3 w-full">
+            <h2 className="sm:w-100 font-bold sm:font-extrabold sm:text-4xl">MISSION</h2>
+          </div>
+          <h3 className="w-50 sm:text-lg sm:w-90 sm:font-semibold pb-6">
+            Shank jerky shoulder short loin corned beef chislic ham cupim. Ribeye andouille jowl, short ribs chislic.
+          </h3>
+        </div>
+        <div className="max-w-screen-md mx-auto sm:w-4/5 flex flex-col px-8 py-8 items-center justify-center">
+          <div className="w-[320px] flex self-start mx-auto pb-3">
+            <img
+              className="w-full"
+              src="https://res.cloudinary.com/dxctpvd8v/image/upload/v1706806762/binoculars_vuhkgz.png"
+              alt="Kid looking through binoculars"
+            />
+          </div>
+          <div className="pb-3 w-full">
+            <h2 className="sm:w-100 font-bold sm:font-extrabold sm:text-4xl">VISION</h2>
+          </div>
+          <h3 className="w-50 sm:text-lg sm:w-90 sm:font-semibold pb-6">
+            Shank jerky shoulder short loin corned beef chislic ham cupim. Ribeye andouille jowl, short ribs chislic.
+          </h3>
+        </div>
+        <div className="max-w-screen-md mx-auto sm:w-4/5 flex flex-col px-8 py-8 items-center justify-center">
+          <div className="w-[320px] flex self-start mx-auto pb-3">
+            <img
+              className="w-full"
+              src="https://res.cloudinary.com/dxctpvd8v/image/upload/v1706806983/crowd_ld0rvy.png"
+              alt="Diversity"
+            />
+          </div>
+          <div className="pb-3 w-full">
+            <h2 className="sm:w-100 font-bold sm:font-extrabold sm:text-4xl">CULTURE</h2>
+          </div>
+          <h3 className="w-50 sm:text-lg sm:w-90 sm:font-semibold pb-6">
+            Shank jerky shoulder short loin corned beef chislic ham cupim. Ribeye andouille jowl, short ribs chislic.
+          </h3>
+        </div>
+      </div>
+    </>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,12 @@ export default {
         sans: ['Montserrat', ...defaultTheme.fontFamily.sans],
       },
     },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
   },
   plugins: [require('@tailwindcss/forms')],
 } satisfies Config;
