b2835e2ff2b34083a5e51ed6ea45eeb9095f7df0
Shamil Khaibullov <sh.khaibullov@gmail.com>
2024-02-08 22:50:15 +0300

    button accepts an icon

--- app/components/ui/button.tsx
+++ app/components/ui/button.tsx
@@ -4,10 +4,11 @@ type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   variant: 'primary' | 'secondary' | 'warm' | 'outlined';
   buttonStyle: 'rounded' | 'fullyRounded';
   children: React.ReactNode;
+  icon?: boolean;
 };
 
-export function Button({ variant, buttonStyle, children, ...props }: ButtonProps) {
-  const baseClasses =
+export function Button({ variant, buttonStyle, icon, children, ...props }: ButtonProps) {
+  let baseClasses =
     'w-full sm:w-fit me-2 mb-2 text-center font-medium focus:outline-none focus:ring-4 px-3 py-2 text-sm sm:px-5 sm:py-2.5 md:py-3 md:text-base';
   let variantClasses = '';
   let styleClasses = '';
@@ -26,6 +27,10 @@ export function Button({ variant, buttonStyle, children, ...props }: ButtonProps
     styleClasses = 'rounded-lg';
   } else {
     styleClasses = 'rounded-full';
+  }
+
+  if (icon) {
+    baseClasses += ' inline-flex items-center justify-center';
   }
 
   return (
