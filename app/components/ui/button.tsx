19f47ff5e67328c1f3990d3460f7e499f0ffcf9b
Shamil Khaibullov <sh.khaibullov@gmail.com>
2024-02-08 23:21:08 +0300

    add disabled prop

--- app/components/ui/button.tsx
+++ app/components/ui/button.tsx
@@ -5,9 +5,10 @@ type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   buttonStyle: 'rounded' | 'fullyRounded';
   children: React.ReactNode;
   icon?: boolean;
+  disabled?: boolean;
 };
 
-export function Button({ variant, buttonStyle, icon, children, ...props }: ButtonProps) {
+export function Button({ variant, buttonStyle, icon, disabled, children, ...props }: ButtonProps) {
   let baseClasses =
     'w-full sm:w-fit me-2 mb-2 text-center font-medium focus:outline-none focus:ring-4 px-3 py-2 text-sm sm:px-5 sm:py-2.5 md:py-3 md:text-base';
   let variantClasses = '';
@@ -31,6 +32,10 @@ export function Button({ variant, buttonStyle, icon, children, ...props }: Butto
 
   if (icon) {
     baseClasses += ' inline-flex items-center justify-center';
+  }
+
+  if (disabled) {
+    variantClasses += ' opacity-75 cursor-not-allowed';
   }
 
   return (
