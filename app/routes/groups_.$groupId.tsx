885b6497c8d563d53ad48857645ac40c46dad7e5
CamilingJS <jonathancamiling@gmail.com>
2024-02-13 22:14:06 -0800

    initiated sytling for groups/groupsId route

--- app/routes/groups_.$groupId.tsx
+++ app/routes/groups_.$groupId.tsx
@@ -3,6 +3,8 @@ import { useParams, useLoaderData, Link } from '@remix-run/react';
 import { db } from '~/modules/database/db.server';
 import { json } from '@remix-run/node';
 
+import { Button } from '~/components/ui/button';
+
 export const loader: LoaderFunction = async ({ params }) => {
   const group = await db.group.findFirstOrThrow({ where: { id: params.groupId } });
   return json({ group });
@@ -12,11 +14,18 @@ export default function GroupRoute() {
   const data = useLoaderData<typeof loader>();
 
   return (
-    <>
-      <Link to="/groups">back to groups</Link>
-      <h1>{data.group.name}</h1>
-      <p>{data.group.description}</p>
-    </>
+    <div className="bg-primary text-white flex flex-col items-center">
+      <div style={{ border: '1px solid red' }} className="md:flex md:flex-row">
+        <div style={{ width: '280px', height: '300px', backgroundColor: 'white', color: 'black' }}>Image</div>
+        <div className="min-w-[280px] p-1">
+          <h1>{data.group.name}</h1>
+          <p>{data.group.description}</p>
+        </div>
+      </div>
+      <Button variant="warm-block" buttonStyle="rounded" size="large">
+        <Link to="/groups">Back to groups</Link>
+      </Button>
+    </div>
   );
 }
 
