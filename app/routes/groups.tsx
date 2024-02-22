be92238647199610c4a1950573f8d24b7112cf94
CamilingJS <jonathancamiling@gmail.com>
2024-02-22 00:08:31 -0800

    added place holder markup for reference

--- app/routes/groups.tsx
+++ app/routes/groups.tsx
@@ -20,8 +20,9 @@ export default function Group() {
                   className="m-1 flex flex-col justify-center items-center"
                   to={group.id}
                 >
-                  <div style={{ width: '210px', borderRadius: '16px' }}>
+                  <div style={{ width: '210px', borderRadius: '16px', display: 'flex', alignSelf: 'flex-start' }}>
                     <img
+                      style={{ backgroundSize: 'cover', backgroundPosition: 'center' }}
                       className="w-full"
                       src="https://images.pexels.com/photos/3184339/pexels-photo-3184339.jpeg"
                       alt="place holder"
