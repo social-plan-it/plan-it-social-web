6b3ac6929ee65567bd0c075a2a3bbbf8aac4b496
CamilingJS <jonathancamiling@gmail.com>
2024-02-22 22:47:38 -0800

    worked on markup

--- app/routes/groups.tsx
+++ app/routes/groups.tsx
@@ -7,32 +7,31 @@ export default function Group() {
   const groups = useGroups();
   return (
     <div className="bg-primary">
-      <div style={{ border: 'solid 2px red' }} className="m-auto max-w-screen-xl">
-        <ul style={{ border: 'solid 2px green' }} className="w-full flex flex-wrap text-secondary">
+      <div className="m-auto max-w-screen-xl">
+        <ul className="w-full flex flex-wrap justify-center text-secondary">
           {groups?.map((group) => {
             return (
-              <li
-                style={{ listStyle: 'none', border: 'solid 3px blue' }}
-                className="flex flex-wrap my-2 mx-auto md:m-2"
-                key={group.id}
-              >
+              <li className="flex flex-wrap justify-center my-2 mx-auto list-none md:m-2" key={group.id}>
                 <Link
-                  style={{ border: 'red 2px solid' }}
-                  className="m-1 flex flex-col justify-center items-center"
+                  style={{ border: '#f5f5f5 1px solid', borderRadius: '1rem' }}
+                  className="m-1 px-4 py-4 flex flex-col justify-center items-center md:w-80 hover:transform hover:scale-105 transition-transform duration-300"
                   to={group.id}
                 >
-                  <div className="w-[13.13rem]">
+                  <div className="w-[13.13rem] h-[13.13rem]">
                     <Image
-                      className="bg-contain bg-center flex self-start rounded-lg"
+                      className="bg-contain bg-center flex self-start rounded-full"
                       src="https://images.pexels.com/photos/3184339/pexels-photo-3184339.jpeg"
                       alt="place holder"
                       background="https://res.cloudinary.com/dxctpvd8v/image/upload/v1708619241/loading_pwxqyd.png"
                       layout="fullWidth"
+                      width={210}
+                      height={210}
                     />
                   </div>
-                  <div className="mx-2">
-                    <h2>{group.name}</h2>
-                    <h3>group-headline-here</h3>
+
+                  <div className="mx-2 my-4">
+                    <h2 className="font-extrabold md:text-xl my-1">{group.name}</h2>
+                    <h3>Group-headline-here</h3>
                   </div>
                 </Link>
               </li>
