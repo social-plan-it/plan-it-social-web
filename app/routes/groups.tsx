ec41aa5d9f3150533baa3e4c88130362a9347321
CamilingJS <jonathancamiling@gmail.com>
2024-03-05 08:43:05 -0800

    made PR suggestions changes

--- app/routes/groups.tsx
+++ app/routes/groups.tsx
@@ -11,26 +11,23 @@ export default function Group() {
         <ul className="w-full flex flex-wrap justify-center text-secondary">
           {groups?.map((group) => {
             return (
-              <li className="flex flex-wrap justify-center my-2 mx-auto list-none md:m-2" key={group.id}>
-                <Link
-                  style={{ border: '#f5f5f5 1px solid', borderRadius: '1rem' }}
-                  className="m-1 px-4 py-4 flex flex-col justify-center items-center md:w-80 hover:transform hover:scale-105 transition-transform duration-300"
-                  to={group.id}
-                >
-                  <div className="w-[13.13rem] h-[13.13rem]">
-                    <Image
-                      className="bg-contain bg-center flex self-start rounded-full"
-                      src="https://images.pexels.com/photos/3184339/pexels-photo-3184339.jpeg"
-                      alt="place holder"
-                      // background="https://res.cloudinary.com/dxctpvd8v/image/upload/v1709096811/SocialPlanit-Loading_qedebk.png"
-                      width={210}
-                      height={210}
-                    />
-                  </div>
-
-                  <div className="mx-2 my-4">
-                    <h2 className="font-extrabold md:text-xl my-1">{group.name}</h2>
-                    <h3>Group-headline-here</h3>
+              <li className="flex flex-wrap justify-center my-2 mx-auto md:m-2" key={group.id}>
+                <Link to={group.id}>
+                  <div className="border-slate-100 border-2 rounded-2xl  m-1 px-4 py-4 flex flex-col justify-center items-center md:w-80 hover:transform hover:scale-105 transition-transform duration-300">
+                    <div className="w-[13.13rem] h-[13.13rem]">
+                      <Image
+                        className="bg-contain bg-center flex self-start rounded-full"
+                        src="https://images.pexels.com/photos/3184339/pexels-photo-3184339.jpeg"
+                        alt="Group meeting around an office table"
+                        background="https://res.cloudinary.com/dxctpvd8v/image/upload/v1709096811/SocialPlanit-Loading_qedebk.png"
+                        width={210}
+                        height={210}
+                      />
+                    </div>
+                    <div className="mx-2 my-4">
+                      <h2 className="font-extrabold md:text-xl my-1">{group.name}</h2>
+                      <h3>Group-headline-here</h3>
+                    </div>
                   </div>
                 </Link>
               </li>
