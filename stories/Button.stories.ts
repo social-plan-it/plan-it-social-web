ef659a775f1fbf2c12fe9ccaebdf7a006fc35374
Andre Landgraf <andre.timo.landgraf@gmail.com>
2024-03-11 21:50:46 -0700

    add storybook, migrate to vite

---
+++ stories/Button.stories.ts
@@ -0,0 +1,57 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { fn } from '@storybook/test';
+import { Button } from './Button';
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+  title: 'Example/Button',
+  component: Button,
+  // parameters: {
+  //   // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+  //   layout: 'centered',
+  // },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ['autodocs'],
+  // More on argTypes: https://storybook.js.org/docs/api/argtypes
+  // argTypes: {
+  //   backgroundColor: { control: 'color' },
+  // },
+  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
+  args: { onClick: fn() },
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const PrimaryRounded: Story = {
+  args: {
+    variant: 'primary',
+    buttonStyle: 'rounded',
+    children: 'Button',
+  },
+};
+
+export const PrimaryFullyRounded: Story = {
+  args: {
+    variant: 'primary',
+    buttonStyle: 'fullyRounded',
+    children: 'Button',
+  },
+};
+
+export const SecondaryRounded: Story = {
+  args: {
+    variant: 'secondary',
+    buttonStyle: 'rounded',
+    children: 'Button',
+  },
+};
+
+export const SecondaryFullyRounded: Story = {
+  args: {
+    variant: 'secondary',
+    buttonStyle: 'fullyRounded',
+    children: 'Button',
+  },
+};
