1b293da4277141657dcc8f35a813895b95afb609
eiffelwong1 <eiffelynx@gmail.com>
2024-03-19 00:01:37 -0700

    lint

--- stories/Button.stories.ts
+++ stories/Button.stories.ts
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { StoryObj } from '@storybook/react';
 import { Button, buttonStyles, variants } from '../app/components/ui/button';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
