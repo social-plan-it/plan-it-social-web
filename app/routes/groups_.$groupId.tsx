944436e489220051c5277b409c7b379a44e963d2
eiffelwong1 <eiffelynx@gmail.com>
2024-03-31 09:17:25 -0700

    nit comments

--- app/routes/groups_.$groupId.tsx
+++ app/routes/groups_.$groupId.tsx
@@ -8,19 +8,13 @@ import { Image } from '~/components/ui/images';
 import { eventsDataPatcher } from '~/modules/events/event';
 
 export const loader: LoaderFunction = async ({ params }) => {
-<<<<<<< HEAD
   const group = await db.group.findFirstOrThrow({ where: { id: params.groupId } });
-  const user_group = await db.user_Group.findMany({ where: { groupId: group.id } });
-  return json({ group, user_group });
-=======
-  const group = await db.group.findFirstOrThrow({ where: { id: params.groupId }, include: { events: true } });
-  return json({ group });
->>>>>>> 3aeb561b07b46bccc12e6e77bc7ed67c2b85fcd6
+  const user_groups = await db.user_Group.findMany({ where: { groupId: group.id } });
+  return json({ group, user_groups });
 };
 
 export default function GroupRoute() {
-  const rawData = useLoaderData<typeof loader>();
-  const group = rawData.group;
+  const { group, user_groups } = useLoaderData<typeof loader>();
   const events = eventsDataPatcher(group.events);
   return (
     <div className="bg-primary text-white flex flex-col items-center">
@@ -92,7 +86,7 @@ export default function GroupRoute() {
                   />
                 </div>
                 <p>
-                  {data.user_group.length} Member{data.user_group.length > 1 ? 's' : ''}
+                  {user_groups.length} Member{user_groups.length > 1 ? 's' : ''}
                 </p>
               </div>
               <div className="bg-white mt-2 text-black rounded-3xl px-4 flex flex-row items-center">
