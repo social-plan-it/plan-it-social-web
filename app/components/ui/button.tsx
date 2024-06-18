d88bbe965ecbebb68f86f813688aa14d869616c2
Shamil Khaibullov <sh.khaibullov@gmail.com>
2024-06-18 14:47:40 +0400

    add AuthButton

--- app/components/ui/button.tsx
+++ app/components/ui/button.tsx
@@ -67,3 +67,37 @@ export function Button({ variant, buttonStyle, icon, disabled, children, ...prop
     </button>
   );
 }
+
+export function AuthButton({ variant, buttonStyle, icon, disabled, children, ...props }: CombinedButtonProps) {
+  let baseClasses = 'w-full text-center font-normal focus:outline-none focus:ring-4 py-3 md:text-base';
+  let variantClasses = '';
+  let styleClasses = '';
+  if (variant === 'primary') {
+    variantClasses = 'bg-primary text-white focus:ring-gray-300 hover:bg-gray-900';
+  }
+  if (variant === 'warm') {
+    variantClasses = 'bg-warm text-white focus:ring-red-300 hover:bg-red-700';
+  }
+  if (variant === 'secondary') {
+    variantClasses = 'bg-secondary focus:ring-teal-300 hover:bg-teal-200';
+  }
+  if (variant === 'outlined') {
+    variantClasses = 'bg-white text-primary border border-gray-200 focus:ring-gray-300 hover:bg-gray-100';
+  }
+  if (icon) {
+    baseClasses += ' inline-flex items-center justify-center';
+  }
+  if (disabled) {
+    variantClasses += ' opacity-75 cursor-not-allowed';
+  }
+  if (buttonStyle === 'rounded') {
+    styleClasses = 'rounded-lg';
+  } else {
+    styleClasses = 'rounded-full';
+  }
+  return (
+    <button type="submit" className={`${baseClasses} ${variantClasses} ${styleClasses}`} {...props}>
+      {children}
+    </button>
+  );
+}
