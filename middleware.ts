b380fe7c866d2dc6617496c0cb95a385ecda3489
Andre Landgraf <andre.timo.landgraf@gmail.com>
2024-07-29 19:33:30 -0700

    fix eslint issues

--- middleware.ts
+++ middleware.ts
@@ -19,7 +19,7 @@ export default async function middleware(request: Request) {
   console.log('running middleware');
   // You could alternatively limit based on user ID or similar
   const ip = ipAddress(request) || '127.0.0.1';
-  const { success, pending, limit, reset, remaining } = await ratelimit.limit(ip);
+  const { success } = await ratelimit.limit(ip);
 
   return success ? next() : Response.redirect(new URL('/blocked.html', request.url));
 }
